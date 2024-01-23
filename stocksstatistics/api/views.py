189df4f85add26fd70d0211aecbd6b7fb430b3a4
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-01-23 11:01:00 +0530

    feat: added docstrings in views.py

--- stocksstatistics/api/views.py
+++ stocksstatistics/api/views.py
@@ -1,10 +1,20 @@
 import pandas as pd
 import numpy as np
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.views.decorators.http import require_POST
 
+@csrf_protect
 def process_data(data):
+    """
+    Process the input DataFrame to calculate daily and annualized volatility.
+
+    Parameters:
+    - data (pd.DataFrame): Input DataFrame with columns ['date', 'open', 'high', 'low', 'close', 'shares_traded', 'turnover'].
+
+    Returns:
+    - Tuple(float, float): Tuple containing daily_volatility and annualized_volatility.
+    """
     column_names = ['date', 'open', 'high', 'low', 'close',
                     'shares_traded', 'turnover']
     data.columns = column_names
@@ -17,8 +27,17 @@ def process_data(data):
 @csrf_exempt
 @require_POST
 def calculate_volatility(request):
-    file_obj = request.FILES['file']
-    data = pd.read_csv(file_obj, header=0)
+    """
+    View function to calculate and return daily and annualized volatility based on the uploaded CSV file.
+
+    Parameters:
+    - request (HttpRequest): The HTTP request object containing the uploaded CSV file.
+
+    Returns:
+    - JsonResponse: JSON response containing daily_volatility and annualized_volatility.
+    """
+    csv_obj = request.FILES['file']
+    data = pd.read_csv(csv_obj, header=0)
     daily_volatility, annualized_volatility = process_data(data)
     return JsonResponse({'daily_volatility': daily_volatility,
                           'annualized_volatility': annualized_volatility
